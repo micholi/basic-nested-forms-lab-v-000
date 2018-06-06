412e18866eda089fee8eaa7bef4a22f38e12d7be
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 13:42:45 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,15 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :ingredient_1,
+      :ingredient_2,
+      :name,
+      :quantity,
+      :ingredient_number
+    ]
+    )
   end
 end
